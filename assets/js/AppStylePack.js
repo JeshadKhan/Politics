f253ca2f6d0a3dbffe0e2ec59d57cb11ba256516
Jeshad Khan <JeshadKhan@users.noreply.github.com>
2017-03-19 02:42:36 +0600

    Upload Project
    Upload web template and all the resources.

---
+++ assets/js/AppStylePack.js
@@ -0,0 +1,39 @@
+/*
+	Document	: Application Style Package
+	Type		: Java Script (JS)
+	Author		: Jeshad Khan (http://www.facebook.com/JeshadKhan.Official)
+	Description	: © 2015.
+*/
+
+
+
+/* Go Back To Top Button */
+
+$(document).ready(function () {
+    // show or hide the sticky footer
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 20) {
+            $('.to-top').fadeIn(500);
+        }
+        else {
+            $('.to-top').fadeOut(700);
+        }
+    });
+    // animate the scroll to top
+    $('.to-top').click(function (event) {
+        event.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, 800);
+    })
+})
+
+
+/* Pretty-Photo Trigger */
+
+$(document).ready(function () {
+
+    $("area[rel^='prettyPhoto']").prettyPhoto();
+
+    $(".gallery:first a[rel^='prettyPhoto']").prettyPhoto({ animation_speed: 'normal', theme: 'light_square', slideshow: 3000, autoplay_slideshow: false, social_tools: "" });
+    $(".gallery:gt(0) a[rel^='prettyPhoto']").prettyPhoto({ animation_speed: 'fast', slideshow: 10000, hideflash: true });
+
+});
